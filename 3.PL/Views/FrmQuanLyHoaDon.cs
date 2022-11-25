44438dae2a26190c7903753a5407445a35c47ea6
bazaizai <hieuhxptto@gmail.com>
2022-11-25 07:55:08 +0700

    Them FrmQLHoaDon

---
+++ 3.PL/Views/FrmQuanLyHoaDon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.PL.Views
+{
+    public partial class FrmQuanLyHoaDon : Form
+    {
+        public FrmQuanLyHoaDon()
+        {
+            InitializeComponent();
+        }
+        private void LoadData()
+        {
+            dataGridView1.ColumnCount = 10;
+        }
+    }
+}
