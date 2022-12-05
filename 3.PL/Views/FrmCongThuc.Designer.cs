493570d0ea672e5d5513e3f1da9aa6824defa2c1
nugetrenkii <mr.zenner203@gmail.com>
2022-12-05 16:57:34 +0700

    thêm tí màu

--- 3.PL/Views/FrmCongThuc.Designer.cs
+++ 3.PL/Views/FrmCongThuc.Designer.cs
@@ -41,12 +41,13 @@
             // 
             // btn_luw
             // 
-            this.btn_luw.BackColor = System.Drawing.Color.LimeGreen;
+            this.btn_luw.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(122)))), ((int)(((byte)(83)))));
             this.btn_luw.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btn_luw.ForeColor = System.Drawing.Color.White;
-            this.btn_luw.Location = new System.Drawing.Point(693, 135);
+            this.btn_luw.Location = new System.Drawing.Point(485, 81);
+            this.btn_luw.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_luw.Name = "btn_luw";
-            this.btn_luw.Size = new System.Drawing.Size(168, 56);
+            this.btn_luw.Size = new System.Drawing.Size(118, 34);
             this.btn_luw.TabIndex = 0;
             this.btn_luw.Text = "Lưu";
             this.btn_luw.UseVisualStyleBackColor = false;
@@ -56,9 +57,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(40, 50);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(28, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 30);
+            this.label1.Size = new System.Drawing.Size(93, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Tỉ lệ quy đổi";
             // 
@@ -66,9 +69,10 @@
             // 
             this.tb_quydoi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tb_quydoi.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.tb_quydoi.Location = new System.Drawing.Point(301, 47);
+            this.tb_quydoi.Location = new System.Drawing.Point(211, 28);
+            this.tb_quydoi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_quydoi.Name = "tb_quydoi";
-            this.tb_quydoi.Size = new System.Drawing.Size(297, 37);
+            this.tb_quydoi.Size = new System.Drawing.Size(208, 27);
             this.tb_quydoi.TabIndex = 2;
             this.tb_quydoi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_quydoi_KeyPress);
             // 
@@ -76,21 +80,24 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(703, 50);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(492, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(158, 30);
+            this.label2.Size = new System.Drawing.Size(107, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "1 điểm thưởng";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.LimeGreen;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(122)))), ((int)(((byte)(83)))));
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(604, 50);
+            this.label3.Location = new System.Drawing.Point(423, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 30);
+            this.label3.Size = new System.Drawing.Size(42, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "VNĐ";
             // 
@@ -98,9 +105,11 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label8.Location = new System.Drawing.Point(673, 50);
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(471, 33);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(28, 30);
+            this.label8.Size = new System.Drawing.Size(19, 20);
             this.label8.TabIndex = 11;
             this.label8.Text = "=";
             // 
@@ -108,9 +117,11 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.radioButton1.Location = new System.Drawing.Point(301, 146);
+            this.radioButton1.ForeColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(211, 88);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(144, 34);
+            this.radioButton1.Size = new System.Drawing.Size(100, 24);
             this.radioButton1.TabIndex = 12;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Hoạt động";
@@ -120,9 +131,11 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.radioButton2.Location = new System.Drawing.Point(464, 146);
+            this.radioButton2.ForeColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(325, 88);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(212, 34);
+            this.radioButton2.Size = new System.Drawing.Size(147, 24);
             this.radioButton2.TabIndex = 13;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Không hoạt động";
@@ -132,18 +145,20 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label9.Location = new System.Drawing.Point(40, 146);
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(28, 88);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(112, 30);
+            this.label9.Size = new System.Drawing.Size(78, 20);
             this.label9.TabIndex = 14;
             this.label9.Text = "Trạng thái";
             // 
             // FrmCongThuc
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(145)))), ((int)(((byte)(224)))), ((int)(((byte)(225)))));
-            this.ClientSize = new System.Drawing.Size(907, 235);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(635, 141);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -153,6 +168,7 @@
             this.Controls.Add(this.tb_quydoi);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_luw);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmCongThuc";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmCongThuc";
